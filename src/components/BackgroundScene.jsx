a0d6682e02ce2145dbe1d9943e00ae55468c6a96
bueno <bruno.gustavo@nave.org.br>
2026-04-15 11:11:55 -0300

    Adjust Stars component saturation and fade speed

--- src/components/BackgroundScene.jsx
+++ src/components/BackgroundScene.jsx
@@ -7,7 +7,7 @@ export function BackgroundScene() {
       <ambientLight intensity={0.1} />
       <pointLight color={C.neon} intensity={1.5} position={[4, 4, 4]} />
       <pointLight color="#ffffff" intensity={0.5} position={[-4, -3, 3]} />
-      <Stars radius={25} depth={12} count={600} factor={2} saturation={0} fade speed={0.8} />
+      <Stars radius={25} depth={12} count={600} factor={2} saturation={0.1} fade speed={0.4} />
     </group>
   );
 }
